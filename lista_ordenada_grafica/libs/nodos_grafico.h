024b6496db74b4a10c8fd2cc05393c9e86a44cb2
Miguel Angel xxxxxxxx yyyyyyyy <chanerec@gmail.com>
2011-04-23 08:21:00 -0500

    se modifico el ejemplo de listas graficas,se hisieron anotaciones pertinentes en los archivos de las listas

--- lista_ordenada_grafica/libs/nodos_grafico.h
+++ lista_ordenada_grafica/libs/nodos_grafico.h
@@ -4,55 +4,102 @@
 
 #define NODO_POS_NODO 0
 
-SDL_Surface *image_nodos, *screen;
-SDL_Rect nodo_rectBlitOrg, nodo_rectBlitDest;
+typedef struct inputs{
+	SDL_Rect insertar, borrar, buscar;
+	int edo_insertar, edo_borrar, edo_buscar;
+	int w,h,y;
+}Inputs;
 
+SDL_Surface *image_nodos, *screen, *img_bg;
+SDL_Rect nodo_rectBlitOrg, nodo_rectBlitDest, rect_screen;
+Inputs inputs;
+/* prototipo de funciones */
+void die(char *s);
+void show_nodo_element();
+void paintBackground();
+void mostrar_nodo(int valor, int pos_x);
+void mostrar_apuntador(int pos_x);
+void mostrar_null(int pos_x);
 
+/*
+ *	Carga los graficos a utilizar durante el trascurso de la ejecución
+ * */
 void cargar_img_nodos(){
 	// Cargamos gráfico
 	image_nodos = IMG_Load("img/nodos.png");
-	if ( image_nodos == NULL ) {
-		printf("No pude cargar gráfico: %s\n", SDL_GetError());
-		exit(1);
-	}
+	if ( image_nodos == NULL )
+		die("No pude cargar gráfico: img/nodos.png\n");
+	//imag de fondo
+	img_bg  = IMG_Load("img/fondo.png");
+	if ( img_bg == NULL )
+		die("No pude cargar gráfico: img/fondo.png\n");
+	//rectangulo que describe el fondo
+	rect_screen.w = SCREEN_W;
+	rect_screen.h = SCREEN_H;
+	rect_screen.x = 0; rect_screen.y = 0;
 	//iniciamos 2 rectangulos los cuales usaremos en los flits
 	//el rect para el bit blitting del origen
-	nodo_rectBlitOrg.w = 100;
-	nodo_rectBlitOrg.h = 100;
+	nodo_rectBlitOrg.w = 50;
+	nodo_rectBlitOrg.h = 50;
 	nodo_rectBlitOrg.y = 0;
 	//el rect para el bit blitting del destino
-	nodo_rectBlitDest.w = 100;
-	nodo_rectBlitDest.h = 100;
-	nodo_rectBlitDest.y = 100;
+	nodo_rectBlitDest.w = 50;
+	nodo_rectBlitDest.h = 50;
+	nodo_rectBlitDest.y = 200;
+	//posiciones y estados iniciales de los inputs
 }
+
+
+void die(char *s){
+	printf("%s\n",s);
+	exit(1);
+}
+
+
+/*
+SDL_Rect rect_setear(int w, int h, int x,int y){
+	SDL_Rect rect;
+	rect.x = w;
+	rect.y = h;
+	rect.y = y;
+	rect.y = y;
+	return rect;
+}
+* */
 
 void show_nodo_element(){
 	SDL_BlitSurface(image_nodos, &nodo_rectBlitOrg, screen, &nodo_rectBlitDest);
 }
 
+void paintBackground(){
+	SDL_Rect rect_aux;
+	SDL_BlitSurface(img_bg, &rect_screen, screen, &rect_screen);
+	rect_aux.x = 100;
+	rect_aux.y = 445;
+	rect_aux.h = 20;
+	rect_aux.w = 80;
+	
+	SDL_FillRect(screen, &rect_aux, SDL_MapRGB(screen->format,200,200,200));
+	rect_aux.x += 200;
+	SDL_FillRect(screen, &rect_aux, SDL_MapRGB(screen->format,200,200,200));
+	rect_aux.x += 200;
+	SDL_FillRect(screen, &rect_aux, SDL_MapRGB(screen->format,200,200,200));
+}
+
 void mostrar_nodo(int valor, int pos_x){
-	//desplazamos el rect del origen(img a pegar)
 	nodo_rectBlitOrg.x = 0;
-	//desplazamos el rect destino en la pos_x(fondo donde se pondra)
 	nodo_rectBlitDest.x = pos_x;
 	show_nodo_element();
 }
 
-
 void mostrar_apuntador(int pos_x){
-	//desplazamos el rect del origen(img a pegar)
-	nodo_rectBlitOrg.x = 100;
-	//desplazamos el rect destino en la pos_x(fondo donde se pondra)
+	nodo_rectBlitOrg.x = 50;
 	nodo_rectBlitDest.x = pos_x;
 	show_nodo_element();
 }
 
 void mostrar_null(int pos_x){
-	//desplazamos el rect del origen(img a pegar)
 	nodo_rectBlitOrg.x = 200;
-	//desplazamos el rect destino en la pos_x(fondo donde se pondra)
 	nodo_rectBlitDest.x = pos_x;
 	show_nodo_element();
 }
-
-
